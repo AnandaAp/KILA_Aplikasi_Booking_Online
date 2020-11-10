2cd8140c2d669cb473981a73ea197263a0a4d1c1
AnandaAp <ananda.apriliansyah@ti.ukdw.ac.id>
2020-11-11 00:42:27 +0700

    Enchantment Feature + menambahkan fitur logic SignIn + menghubungkan dengan Firebase untuk SignUp dan SignIn

--- app/src/main/java/com/ndaktau/kila/login_fragment.java
+++ app/src/main/java/com/ndaktau/kila/login_fragment.java
@@ -1,12 +1,6 @@
 package com.ndaktau.kila;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,20 +9,20 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputLayout;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
-import java.util.Objects;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login_fragment extends Fragment {
     private final String TAG = "com.ndaktau.kila";
-    private Button btnSignIn;
     private TextView signUp;
-    protected TextInputLayout email;
-    protected TextInputLayout password;
+    private TextInputLayout email,password;
     private Button sign_in;
-    private TextView sign_up;
-    protected String email_value = "";
-    protected String password_value = "";
+    protected String email_value,password_value;
+    private FirebaseAuth mAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +36,7 @@ public class login_fragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         signUp = view.findViewById(R.id.signUp);
         signUp.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.NavToSignUp));
+        mAuth = FirebaseAuth.getInstance();
         NavToSignUp();
         init(view);
         loginCheck();
@@ -51,87 +46,40 @@ public class login_fragment extends Fragment {
         email = view.findViewById(R.id.emailInputLayout);
         password = view.findViewById(R.id.passwordInputLayout);
         sign_in = view.findViewById(R.id.signIn);
-        sign_up = view.findViewById(R.id.signUp);
-//        Objects.requireNonNull(email.getEditText()).addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                if (s.length() == 0 ) {
-//                    email.setError("Email Belum Terisi");
-//                    email.setErrorEnabled(true);
-//                }
-//                else {
-//                    email.setErrorEnabled(false);
-//                }
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//                email_value = email.getEditText().getText().toString();
-//            }
-//        });
-//        Objects.requireNonNull(password.getEditText()).addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                if(s.length() == 0){
-//                    password.setError("Password Belum diisi");
-//                    password.setErrorEnabled(true);
-//                }
-//                else{
-//                    password.setErrorEnabled(false);
-//                }
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//                password_value = password.getEditText().getText().toString();
-//            }
-//        });
     }
 
     private  void loginCheck(){
         sign_in.setOnClickListener(v -> {
-            if(email_value.equals("admin") && password_value.equals("admin")){
-                Toast.makeText(requireContext(),"Selamat anda sudah login",Toast.LENGTH_LONG).show();
+            email_value = email.getEditText().getText().toString().trim();
+            password_value = password.getEditText().getText().toString().trim();
+            if(email_value.isEmpty()){
+                email.setError("Email tidak boleh kosong");
+                email.setErrorEnabled(true);
+            }
+            else if(password_value.isEmpty()){
+                password.setError("Password tidak boleh kosong");
+                password.setErrorEnabled(true);
+            }
+            else{
                 email.setErrorEnabled(false);
                 password.setErrorEnabled(false);
-                Log.i(TAG, "onClick: "+email_value);
-                Log.i(TAG, "onClick: "+password_value);
-                Log.i(TAG, "onClick: berhasil login");
-            }
-            else if(email_value.length() > 0 && password_value.length() == 0){
-                password.setError("Harap mengisi password terlebih dahulu");
-                password.setErrorEnabled(true);
-            }
-            else if(email_value.length() == 0 && password_value.length() > 0){
-                email.setError("Harap mengisi email terlebih dahulu");
-                email.setErrorEnabled(true);
-            }
-            else if(email_value.length() == 0 && password_value.length() == 0){
-                Toast.makeText(requireContext(),"Harap mengisi Input Terlebih Dahulu",
-                        Toast.LENGTH_LONG).show();
-                email.setErrorEnabled(true);
-                password.setErrorEnabled(true);
-            }
-            else if(email_value.equals("admin") && !password_value.equals("admin")){
-                Toast.makeText(requireContext(),"Email atau Password yang anda " +
-                        "salah",Toast.LENGTH_LONG).show();
-            }
-            else if(!email_value.equals("admin") && password_value.equals("admin")){
-                Toast.makeText(requireContext(),"Email atau Password yang anda " +
-                        "salah",Toast.LENGTH_LONG).show();
-            }
-            else if(!email_value.equals("admin") && !password_value.equals("admin")){
-                Toast.makeText(requireContext(),"Email atau Password yang anda " +
-                        "salah",Toast.LENGTH_LONG).show();
+                mAuth.signInWithEmailAndPassword(email_value, password_value)
+                        .addOnCompleteListener(requireActivity(), task -> {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(requireActivity(),"Login "+user.getEmail()
+                                        ,Toast.LENGTH_LONG).show();
+//                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(requireActivity(), "Email atau Password salah",
+                                        Toast.LENGTH_SHORT).show();
+//                                updateUI(null);
+                            }
+                        });
             }
         });
     }
