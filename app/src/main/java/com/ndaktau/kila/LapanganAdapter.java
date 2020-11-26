03e9884b871e91290d545a02d56766065fe92629
RuselAlxndr <Rusel.alexander@ti.ukdw.ac.id>
2020-11-27 01:19:39 +0700

    progress read data lapangan user belum selesai

---
+++ app/src/main/java/com/ndaktau/kila/LapanganAdapter.java
@@ -0,0 +1,128 @@
+package com.ndaktau.kila;
+
+import android.content.Context;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import static android.graphics.text.LineBreaker.JUSTIFICATION_MODE_INTER_WORD;
+
+public class LapanganAdapter extends RecyclerView.Adapter<LapanganAdapter.kategoriviewholder>{
+    private String[] jenislapangan;
+    private String[] nama;
+    private String[] harga;
+    private String[] kontak;
+    private String[] alamat;
+
+    private Context context;
+
+    public LapanganAdapter(String[] jenislapangan, String[] nama, String[] harga, String[] kontak,String[] alamat, Context context) {
+        setJenislapangan(jenislapangan);
+        setHarga(harga);
+        setKontak(kontak);
+        setNama(nama);
+        setContext(context);
+        setAlamat(alamat);
+
+
+    }
+
+    public String[] getJenislapangan() {
+        return jenislapangan;
+    }
+
+    public String[] getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String[] alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setJenislapangan(String[] jenislapangan) {
+        this.jenislapangan = jenislapangan;
+    }
+
+    public String[] getNama() {
+        return nama;
+    }
+
+    public void setNama(String[] nama) {
+        this.nama = nama;
+    }
+
+    public String[] getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String[] harga) {
+        this.harga = harga;
+    }
+
+    public String[] getKontak() {
+        return kontak;
+    }
+
+    public void setKontak(String[] kontak) {
+        this.kontak = kontak;
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public kategoriviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.single_row_design,parent,false);
+        return new kategoriviewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull kategoriviewholder holder, int position) {
+        holder.nama.setText(getNama()[position]);
+        holder.harga.setText(getHarga()[position]);
+        holder.alamat.setText(getAlamat()[position]);
+        holder.kontak.setText(getKontak()[position]);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            holder.nama.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+            holder.harga.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+            holder.alamat.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+            holder.kontak.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return nama.length;
+    }
+
+    public static class kategoriviewholder extends RecyclerView.ViewHolder{
+        TextView nama,alamat,harga,kontak;
+
+
+        public kategoriviewholder(@NonNull View itemView) {
+            super(itemView);
+            nama = itemView.findViewById(R.id.namalapangan);
+            harga = itemView.findViewById(R.id.harga);
+            alamat= itemView.findViewById(R.id.alamat);
+            kontak = itemView.findViewById(R.id.kontak);
+
+
+        }
+    }
+
+}
